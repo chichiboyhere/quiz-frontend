9e9d42e5dbe9c3215264a8dd165dbc3de52e33cd
Tony <tonyjays@yahoo.com>
2023-12-01 08:28:53 +0100

    adjusted the index.js file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import "./index.css"
 import { AuthContextProvider } from "./context/AuthContext";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-import { store } from "./app/store";
+
 
 if (process.env.REACT_APP_NODE_ENV==='production'){
   disableReactDevTools();
@@ -13,10 +13,8 @@ if (process.env.REACT_APP_NODE_ENV==='production'){
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <AuthContextProvider>
           <App />
       </AuthContextProvider>
-    </Provider>
   </React.StrictMode>
 );
